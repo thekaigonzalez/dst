4d55ad38b3a3f585c87cb8084fd332e5279839d3
Kai D. Gonzalez <gkai70263@gmail.com>
2023-10-11 20:30:34 -0400

    big changes to the string header

--- string.h
+++ string.h
@@ -5,14 +5,114 @@
 
 typedef struct string
 {
-  char *_p; // inner string
-  int _s;   // the position of the string
+  char *_p;       // inner string
+  int __position; // the position of the string
+
+  int __index; // the index of the current character
+  char curr;   // the current character
+  char prev;   // the previous character
+  char next;   // the next character
 } string;
 
-void string_append (string *s, char z);
+typedef struct string_list
+{
+  string _list[100];
+  int _size;
+} string_list;
 
-void string1 (string *s);
+// add a character to the string
+void str_app (string *s, char z);
 
-void puts (string *s, const char *str);
+/// @brief initializes the string
+/// @param s 
+void str_init (string *s);
+
+// adds *str into the string (can also be used as a concatenate feature if
+// string isn't cleared, to clear them use str_xmove_replace())
+void str_xmove (string *s, const char *str);
+
+// adds *str into the string, replacing the current string
+void str_xmove_replace (string *s, const char *str);
+
+// frees the string
+void str_free (string *s);
+
+// removes the last character from the string
+char str_pop (string *s);
+
+// returns the length of the string
+int str_length (string *s);
+
+// removes a character at POS in the string
+void str_remove (string *s, int pos);
+
+// gets character at position in the string
+char str_get (string *s, int pos);
+
+// returns the string as a char*
+char *str_to_str (string *s);
+
+// copies a string into another string
+void str_cpy (string *dst, string *src);
+
+// checks if one string is equal to another
+int str_equal (string *s1, string *s2);
+
+// returns the current index
+int str_index (string *s);
+
+// moves to the next character
+void str_next (string *s);
+
+// checks the next and previous characters, also updating the current character
+void str_where (string *s);
+
+// checks if the index exists in the string
+int str_exists (string *s, int index);
+
+// returns the current character at the token index
+char str_current (string *s);
+
+// returns the previous character
+char str_prev (string *s);
+
+// returns the next character
+char str_pnext (string *s);
+
+// prints a string
+void str_print (string *s);
+
+// strips the left side of the string
+void str_strip_left (string *s);
+
+// strips the right side of the string
+void str_strip_right (string *s);
+
+// strips the left and right side of the string
+void str_strip (string *s);
+
+// will convert the string to uppercase
+void str_uppercase (string *s);
+
+// will convert the string to lowercase
+void str_lowercase (string *s);
+
+// switches the case of the string (turns lower case to upper case and vice versa)
+void str_switch_case (string *s);
+
+// splits the string into an array of strings
+string_list str_split (string *s, char delim);
+
+// sets the string to empty
+void str_empty (string *s);
+
+// starts a string list
+void str_list_init (string_list *s);
+
+// returns the size of the string list
+int str_list_size (string_list *s);
+
+// adds a string to the string list
+void str_list_add (string_list *s, string *str);
 
 #endif
