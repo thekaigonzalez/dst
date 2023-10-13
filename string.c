cc789ce6b3d00b50622c07e20890772d2be5db3c
Kai D. Gonzalez <gkai70263@gmail.com>
2023-10-12 21:26:29 -0400

    extended the library adding more robust memory management and security features

--- string.c
+++ string.c
@@ -16,16 +16,16 @@
     }
 
 void
-str_app (string *s, char z)
+str_append_last (string *s, char z)
 {
-  s->_p = realloc (s->_p, ALLOC_ESCAPE (str_length (s))
-                              + 1); // allocate a new string with the same
-                                    // length (with 2 extra bytes)
-  VALIDATE (s->_p);
-  s->_p[s->__position] = z; // set the current index to the character
+  s->ptr = realloc (s->ptr, ALLOC_ESCAPE (str_length (s))
+                                + 1); // allocate a new string with the same
+                                      // length (with 2 extra bytes)
+  VALIDATE (s->ptr);
+  s->ptr[s->__position] = z; // set the current index to the character
 
   if (z != '\0')
-    s->_p[(s->__position) + 1] = 0; // escape it too
+    s->ptr[(s->__position) + 1] = 0; // escape it too
 
   s->__position++; // update the index
 }
@@ -35,10 +35,14 @@ str_app (string *s, char z)
 void
 str_init (string *s)
 {
+  // if (s->ptr != NULL) // free the old memory
+  //   {
+  //     free (s->ptr);
+  //   }
   s->__position = 0;
-  s->_p = malloc (1);
-  VALIDATE (s->_p);
-  s->_p[0] = 0;
+  s->ptr = malloc (1);
+  VALIDATE (s->ptr);
+  s->ptr[0] = 0;
 
   s->__index = 0;
 
@@ -47,33 +51,31 @@ str_init (string *s)
 
 // puts a string into the structure using string_append()
 void
-str_xmove (string *s, const char *str)
+str_assign_literal (string *s, const char *str)
 {
   while (*str)
     {
-      str_app (s, *str);
+      str_append_last (s, *str);
       str++;
     }
 }
 
-// str_xmove, except the string is re-initiated before added the new string on
-// top of it, allowing you to have a full string instead of that string being
-// added onto it
-// note: this does NOT replace words in the string
+// str_assign_literal, except the string is re-initiated before added the new
+// string on top of it, allowing you to have a full string instead of that
+// string being added onto it note: this does NOT replace words in the string
 void
-str_xmove_replace (string *s, const char *str)
+str_assign_literal_replace (string *s, const char *str)
 {
   str_init (s);
 
-  str_xmove (s, str);
+  str_assign_literal (s, str);
 }
 
 // frees the string
 void
 str_free (string *s)
 {
-  free (s->_p);
-  str_empty (s); // set everything back to 0
+  str_void (s);
 }
 
 // removes the last character in the string and returns it
@@ -83,7 +85,6 @@ str_pop (string *s)
   char n = str_get (s, str_length (s) - 1);
 
   str_remove (s, str_length (s) - 1);
-
   return n;
 }
 
@@ -96,7 +97,8 @@ str_length (string *s)
 void
 str_remove (string *s, int pos)
 {
-  VALIDATE (memmove (s->_p + pos, s->_p + pos + 1, str_length (s) - pos));
+  VALIDATE (memmove (s->ptr + pos, s->ptr + pos + 1, str_length (s) - pos));
+  str_pos_move (s, -1);
 }
 
 // returns character
@@ -107,23 +109,23 @@ str_get (string *s, int pos)
     {
       fprintf (stderr, "Index out of bounds\n");
     };
-  return s->_p[pos];
+  return s->ptr[pos];
 }
 
 char *
 str_to_str (string *s)
 {
-  return s->_p;
+  return s->ptr;
 }
 
 void
 str_cpy (string *dst, string *src)
 {
   dst->__position = src->__position;
-  dst->_p = malloc (ALLOC_ESCAPE (src->__position));
-  VALIDATE (dst->_p);
+  dst->ptr = malloc (ALLOC_ESCAPE (src->__position));
+  VALIDATE (dst->ptr);
 
-  memcpy (dst->_p, src->_p, src->__position);
+  memcpy (dst->ptr, src->ptr, src->__position);
 }
 
 int
@@ -156,12 +158,12 @@ str_next (string *s)
 void
 str_where (string *s)
 {
-  s->curr = s->_p[s->__index];
+  s->curr = s->ptr[s->__index];
   if (str_exists (s, str_index (s) - 1))
-    s->prev = s->_p[s->__index - 1];
+    s->prev = s->ptr[s->__index - 1];
 
   if (str_exists (s, str_index (s) + 1))
-    s->next = s->_p[s->__index + 1];
+    s->next = s->ptr[s->__index + 1];
 }
 
 int
@@ -174,19 +176,19 @@ char
 str_current (string *s)
 {
   if (str_index (s) < str_length (s))
-    return s->_p[s->__index];
+    return s->ptr[s->__index];
 }
 
 char
 str_prev (string *s)
 {
-  return (str_exists (s, str_index (s) - 1)) ? s->_p[s->__index - 1] : '\0';
+  return (str_exists (s, str_index (s) - 1)) ? s->ptr[s->__index - 1] : '\0';
 }
 
 char
 str_pnext (string *s)
 {
-  return (str_exists (s, str_index (s) + 1)) ? s->_p[s->__index + 1] : '\0';
+  return (str_exists (s, str_index (s) + 1)) ? s->ptr[s->__index + 1] : '\0';
 }
 
 void
@@ -201,14 +203,14 @@ str_strip_left (string *s)
   for (int i = 0; i < str_length (s) - 1; i++)
     {
 
-      if (isspace (s->_p[i]))
+      if (isspace (s->ptr[i]))
         {
           str_remove (s, i);
           i--;
           continue;
         }
 
-      else if (!isspace (s->_p[i]))
+      else if (!isspace (s->ptr[i]))
         break;
     }
 }
@@ -218,12 +220,12 @@ str_strip_right (string *s)
 {
   for (int i = str_length (s) - 1; i > 0; i--)
     {
-      if (isspace (s->_p[i]))
+      if (isspace (s->ptr[i]))
         {
           str_remove (s, i);
           continue;
         }
-      else if (!isspace (s->_p[i]) && s->_p[i] != '\0')
+      else if (!isspace (s->ptr[i]) && s->ptr[i] != '\0')
         break;
     }
 }
@@ -239,14 +241,14 @@ void
 str_uppercase (string *s)
 {
   for (int i = 0; i < str_length (s); i++)
-    s->_p[i] = toupper (s->_p[i]);
+    s->ptr[i] = toupper (s->ptr[i]);
 }
 
 void
 str_lowercase (string *s)
 {
   for (int i = 0; i < str_length (s); i++)
-    s->_p[i] = tolower (s->_p[i]);
+    s->ptr[i] = tolower (s->ptr[i]);
 }
 
 void
@@ -254,11 +256,11 @@ str_switch_case (string *s)
 {
   for (int i = 0; i < str_length (s); i++)
     {
-      if (islower (s->_p[i]))
-        s->_p[i] = toupper (s->_p[i]);
+      if (islower (s->ptr[i]))
+        s->ptr[i] = toupper (s->ptr[i]);
 
-      else if (isupper (s->_p[i]))
-        s->_p[i] = tolower (s->_p[i]);
+      else if (isupper (s->ptr[i]))
+        s->ptr[i] = tolower (s->ptr[i]);
     }
 }
 
@@ -275,17 +277,17 @@ str_split (string *s, char delim)
 
   for (int i = 0; i < str_length (s); i++)
     {
-      if (s->_p[i] == delim)
+      if (s->ptr[i] == delim)
         {
         add:
           str_list_add (&splits, &buffer);
-          str_empty (&buffer);
-
+          str_void (&buffer);
+          str_init(&buffer);
           n++;
         }
       else
         {
-          str_app (&buffer, s->_p[i]);
+          str_append_last (&buffer, s->ptr[i]);
         }
     }
 
@@ -293,6 +295,9 @@ str_split (string *s, char delim)
     {
       goto add;
     }
+  
+  str_void(&buffer);
+
   return splits;
 }
 
@@ -304,8 +309,8 @@ str_empty (string *s)
   s->curr = '\0';
   s->prev = '\0';
   s->next = '\0';
-  s->_p = malloc (1);
-  s->_p[0] = 0;
+  s->ptr = malloc (1);
+  s->ptr[0] = 0;
 }
 
 void
@@ -323,6 +328,9 @@ str_list_size (string_list *s)
 void
 str_list_add (string_list *s, string *str)
 {
+  if (&s->_list[s->_size] == NULL)
+    return; // the string list is either full or the size isn't correct, one of
+            // the two
   str_cpy (&s->_list[s->_size], str);
 
   s->_size++;
@@ -347,5 +355,158 @@ str_at (string *s, int pos)
   if (pos >= str_length (s))
     fprintf (stderr, "Index out of bounds\n");
 
-  return s->_p[pos];
+  return s->ptr[pos];
+}
+
+void
+str_pos_move (string *s, int n)
+{
+  if (s->__position + n > 0)
+    {
+      s->__position += n;
+    }
+}
+
+int
+str_null (string *s)
+{
+  return s->ptr == NULL;
+}
+
+string *
+str_list_get_at (string_list *s, int index)
+{
+  if (index >= str_list_size (s))
+    return NULL;
+
+  return &s->_list[index];
+}
+
+void
+str_list_free (string_list *s)
+{
+  for (int i = 0; i < str_list_size (s); i++)
+    {
+      str_void (&s->_list[i]);
+    }
+}
+
+void
+str_list_remove (string_list *s, int index)
+{
+  if (index >= str_list_size (s)) return;
+
+  str_free (&s->_list[index]);
+  str_void(&s->_list[index]);
+}
+
+void
+str_void (string *s)
+{
+  free (s->ptr);
+  s->ptr = NULL;
+  s = NULL;
+}
+
+string
+str_impostor (string *s)
+{
+  string new;
+  str_init (&new);
+  str_cpy(s, &new);
+  return new;
+}
+
+void
+str_replace (string *s, char old, char new)
+{
+  for (int i = 0; i < str_length (s); i++)
+    {
+      if (s->ptr[i] == old)
+        s->ptr[i] = new;
+    }
+}
+
+void
+str_replace_long (string *s, string *old, string *new)
+{
+  int start = 0;
+  if (old == NULL || new == NULL) return;
+
+  for (int i = 0; i < str_length (s); i++) {
+    if (start > str_length(old)) {
+      break;
+    }
+    if (str_at(s, i) == old->ptr[start]) {
+      str_swap(s, i, new->ptr[start]);
+      start++;
+    }
+  }
+}
+
+string*
+str_distinct (const char *str)
+{
+  string* s = malloc (sizeof (string));
+  // str_init (s);
+  str_assign_literal_replace(s, str);
+
+  return s;
+}
+
+void
+str_void_ptr (string *s)
+{
+  free (s->ptr);
+  s->ptr = NULL;
+  free(s);
+  s = NULL;
+}
+
+void
+str_void_ptr_gc (string *s)
+{
+  free (s->ptr);
+  s->ptr = NULL;
+  s = NULL;
+}
+
+gc_string
+str_gc_empty (void)
+{
+  string l;
+  str_init (&l);
+  return l;
+}
+
+string *
+str_substr (string *s, int start, int end)
+{
+  string *str = str_distinct("");
+
+  if (start < 0 || end > str_length(s))
+    return NULL;
+
+  for (int i = start; i < end; i++)
+    str_append_last (str, s->ptr[i]);
+
+  return str;
+}
+
+void
+str_escape (string *s)
+{
+  
+}
+
+void
+str_unescape (string *s)
+{
+}
+
+void
+str_swap (string *s, int i, char j)
+{
+  if (i > str_length (s)) return;
+  s->ptr[i] = j;
 }
