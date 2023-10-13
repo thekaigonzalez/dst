17d2072dd79700b503b20b45348086b8b3dd64b2
Kai D. Gonzalez <gkai70263@gmail.com>
2023-10-12 21:26:47 -0400

    test script

--- test.sh
+++ test.sh
@@ -1,6 +1,28 @@
 #Copyright 2019-2023 Kai D. Gonzalez
 
-gcc test.c -o test string.c -Wno-implicit-int
+#--- valgrind
+gcc test.c -o test string.c
 clear
+valgrind --leak-check=full -s ./test
+read -n 1 -s -r -p "Press any key to continue . . ."
+rm ./test
+
+#--- testing with gcc
+
+gcc test.c -o test string.c -Wno-implicit-int -fsanitize=address -g -O3
+clear
+printf "gcc outputs:\n";
 ./test
 rm test
+
+sleep 2;
+
+
+#--- testing with tcc
+tcc test.c -o test string.c
+clear
+printf "tcc outputs:\n";
+./test
+rm test
+
+sleep 2
